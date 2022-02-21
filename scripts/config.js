6098249553a0bf280e67ff5110f7d370eb58d9f2
sumitkv31 <sumitkv31@gmail.com>
2022-02-21 17:22:21 +0530

    working in javascript dom

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,6 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid; //+'string' will be converted to number
+
   playerConfigOverlayElement.style.display = "block";
   backdropElement.style.display = "block";
 }
@@ -8,6 +10,9 @@ function closePlayerConfig() {
   backdropElement.style.display = "none";
   formElement.firstElementChild.classList.remove("error");
   errorOutputElement.textContent = "";
+
+  //getting input html and setting its value to empty when closing the form
+  formElement.firstElementChild.lastElementChild.value = "";
 }
 
 function savePlayerConfig(event) {
@@ -26,4 +31,19 @@ function savePlayerConfig(event) {
     errorOutputElement.textContent = "please enter a valid name!";
     return;
   }
+
+  //constructing the id dynamically based on whether it is for player 1 or player 2
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
+
+  updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+  if (editedPlayer === 1) {
+    players[0].name = enteredPlayername;
+  } else {
+    players[1].name = enteredPlayername;
+  }
+
+  closePlayerConfig();
 }
